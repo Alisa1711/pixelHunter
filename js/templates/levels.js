ff14aeb2f78f3330abf925b66b4bfcb3505284e6
Alisa <alisa.sp@gmail.com>
2019-01-27 18:10:37 +0300

    добавляет resize фотографий

--- js/templates/levels.js
+++ js/templates/levels.js
@@ -1,16 +1,40 @@
 import {playGame} from '../main';
+import {resize} from '../utils';
+
+const DOUBLE_FRAME = {
+  width: 468,
+  height: 458
+};
+const SINGLE_FRAME = {
+  width: 705,
+  height: 455
+};
+const TRIPLE_FRAME = {
+  width: 304,
+  height: 455
+};
+
+const resizeImages = (levelElement) => {
+  const images = Array.from(levelElement.querySelectorAll(`.game__option img`));
+  images.forEach((img) => {
+    img.addEventListener(`load`, () => {
+      const newSize = resize({width: img.width, height: img.height}, {width: img.naturalWidth, height: img.naturalHeight});
+      img.width = newSize.width;
+      img.height = newSize.height;
+    });
+  });
+};
+
 export default {
   photoOrPaint: {
     getString(level) {
-      const wide = level.images.length === 1;
-      const image = {
-        width: wide ? 705 : 468,
-        height: wide ? 455 : 458
-      };
+      const isOneImg = level.images.length === 1;
+      const frame = isOneImg ? SINGLE_FRAME : DOUBLE_FRAME;
+
       return `<p class="game__tassk">${level.task}</p>
-      <form class="game__content ${wide ? `game__content--wide` : ``}">
+      <form class="game__content ${isOneImg ? `game__content--wide` : ``}">
       ${[...level.images].map((img, i) => `<div class="game__option">
-          <img src="${img.src}" alt="Option ${i}" width="${image.width}" height="${image.height}">
+          <img src="${img.src}" alt="Option ${i}" width="${frame.width}" height="${frame.height}">
           <label class="game__answer game__answer--photo">
             <input class="visually-hidden" name="question${i}" type="radio" value="photo">
             <span>Фото</span>
@@ -23,6 +47,8 @@ export default {
         </form>`;
     },
     setListener(levelElement, game) {
+      resizeImages(levelElement);
+
       const answers = Array.from(levelElement.querySelectorAll(`input[type=radio]`));
       const options = levelElement.querySelectorAll(`.game__option`);
 
@@ -44,11 +70,13 @@ export default {
       return `<p class="game__task">${level.task}</p>
         <form class="game__content  game__content--triple">
         ${[...level.images].map((img, i) => `<div class="game__option">
-        <img src="${img.src}" alt="Option ${i}" width="304" height="455">
+        <img src="${img.src}" alt="Option ${i}" width="${TRIPLE_FRAME.width}" height="${TRIPLE_FRAME.height}">
       </div>`).join(``)}
         </form>`;
     },
     setListener(levelElement, game) {
+      resizeImages(levelElement);
+
       const answers = levelElement.querySelectorAll(`.game__option img`);
       const onAnswerClick = ({target}) => {
         const selectedPicture = game.levelObj.images.find(
